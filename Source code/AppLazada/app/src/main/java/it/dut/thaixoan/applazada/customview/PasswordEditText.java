b81ecac1e9ea15d51de2891b8de1a8bd4a770aab
Tran Thai Xoan <xoan.tranthai@ncc.asia>
2019-03-05 18:48:14 +0700

    custom edittext pass

--- Source code/AppLazada/app/src/main/java/it/dut/thaixoan/applazada/customview/PasswordEditText.java
+++ Source code/AppLazada/app/src/main/java/it/dut/thaixoan/applazada/customview/PasswordEditText.java
@@ -1,4 +1,24 @@
 package it.dut.thaixoan.applazada.customview;
 
-public class PasswordEditText {
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+
+
+public class PasswordEditText extends android.support.v7.widget.AppCompatEditText {
+
+    Drawable eyeShow;
+    Drawable eyeHint;
+    Boolean  visible = false;
+    public PasswordEditText(Context context) {
+        super(context);
+    }
+
+    public PasswordEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PasswordEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 }
